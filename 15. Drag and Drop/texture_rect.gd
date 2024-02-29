dcbc48521eaf67d56b148e7252a0ced802ce87bd
tigrau <unstoppabletigra@gmail.com>
2024-02-29 21:35:08 +0300

    Drag and Drop

---
+++ 15. Drag and Drop/texture_rect.gd
@@ -0,0 +1,23 @@
+extends TextureRect
+
+func _get_drag_data(at_position: Vector2) -> Variant:
+	
+	var preview_texture = TextureRect.new()
+	
+	preview_texture.texture = texture
+	preview_texture.expand_mode = 1
+	preview_texture.size = Vector2(30,30)
+	
+	var  preview = Control.new()
+	preview.add_child(preview_texture)
+	
+	set_drag_preview(preview)
+	
+	texture = null
+	return preview_texture.texture
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return data is Texture2D
+	
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	texture = data
